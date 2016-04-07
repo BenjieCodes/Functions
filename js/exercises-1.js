9873ecd620b9b34524d9ea6f25b328532d8b5b61
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-06 22:06:29 -0400

    Finished exercise-1.js (not sure if answer for Q6 is correct)

---
+++ js/exercises-1.js
@@ -0,0 +1,96 @@
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+////    ______                _   _                ////
+////    |  ___|              | | (_)               ////
+////    | |_ _   _ _ __   ___| |_ _  ___  _ __     ////
+////    |  _| | | | '_ \ / __| __| |/ _ \| '_ \    ////
+////    | | | |_| | | | | (__| |_| | (_) | | | |   ////
+////    \_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|   ////
+////                                               ////
+////                                               ////
+////    ______               _   _                 ////
+////    | ___ \             | | (_)                ////
+////    | |_/ / __ __ _  ___| |_ _  ___ ___        ////
+////    |  __/ '__/ _` |/ __| __| |/ __/ _ \       ////
+////    | |  | | | (_| | (__| |_| | (_|  __/       ////
+////    \_|  |_|  \__,_|\___|\__|_|\___\___|       ////
+////                                               ////
+////                                               ////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+function sum (x, y) {
+  var z = x + y;
+  return z;
+}
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+function avg (x , y , z) {
+  var sum = x + y + z;
+  var answer = sum / 3;
+  return answer;
+}
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+function getLength (string) {
+  var length = string.length
+  return length;
+}
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+function greaterThan (x, y) {
+  var z = y > x;
+  return z;
+}
+
+
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+function greet (name) {
+  var hi = "Hello, " + name + "!";
+  return hi;
+}
+
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+
+function madlib (a, b, c, d) {
+  var sentence = a + " " + b + " " + c + " " + d;
+  return sentence;
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
